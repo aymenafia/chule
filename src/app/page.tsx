83783d6e9726781a03ed3e715501b64eb5af47ca
Aymen Afia <Aymen@ChargeAtFriends.com>
2024-05-31 14:50:44 +0200

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <h1 className="text-6xl font-bold">Hello World👋</h1>
-    </main>
-  )
+    <div>
+      <main className="flex min-h-screen flex-col items-center justify-center">
+        <h1 className="text-6xl font-bold">Hello to quizz page👋</h1>
+      </main>
+
+      <footer>
+        <Button>Start</Button>
+      </footer>
+    </div>
+  );
 }
