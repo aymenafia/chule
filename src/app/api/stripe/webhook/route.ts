e6c388480285abb2126dc668746668ba20c12f1b
Aymen Afia <aymenafia@Aymens-MBP.fritz.box>
2024-06-02 21:25:55 +0200

    initial comit

--- src/app/api/stripe/webhook/route.ts
+++ src/app/api/stripe/webhook/route.ts
@@ -15,8 +15,7 @@ const relevantEvents = new Set([
 export async function POST(req: Request) {
   const body = await req.text();
   const sig = req.headers.get("stripe-signature") as string;
-  const webHookSecret =
-    "whsec_1ae6611b0e7c86ef2db7d77a743bae91db349e3c0da48145fb6e6557d57b7df1";
+  const webHookSecret = process.env.NODE_ENV === "production" ? process.env.STRIPE_WEBHOOK_SECRET : process.env.STRIPE_WEBHOOK_LOCAL_SECRET;
 
   if (!webHookSecret) {
     throw new Error("STRIPE_WEBHOOK_SECRET is not set");
