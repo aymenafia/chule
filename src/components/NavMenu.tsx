8feb4af8d794ef5c3f11285fdb71283cf90a113c
Aymen Afia <aymenafia@Aymens-MBP.fritz.box>
2024-06-02 21:25:55 +0200

    initial comit

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -75,6 +75,7 @@ export function NavMenu() {
           <span>Settings</span>
         </DropdownMenuItem> */}
       </DropdownMenuGroup>
+
       {/* <DropdownMenuSeparator /> */}
       {/* <DropdownMenuGroup>
         <DropdownMenuSub>
